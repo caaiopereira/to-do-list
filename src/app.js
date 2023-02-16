1c4bc6edc1a8e607cd1acc958ab17ec001f6de2b
caaiopereira <caaiopereira9@gmail.com>
2023-02-16 19:36:48 -0300

    Add files via upload

--- src/app.js
+++ src/app.js
@@ -4,14 +4,22 @@ const express = require('express')
 // instanciando o servidor
 const app = express()
 
+const cors = require('cors')
+
 // configurando o servidor para receber requisições com o corpo no formato JSON
 app.use(express.json())
 
-// importando os controllers
-const usuarioController = require('./controllers/usuarioController.js')
-const tarefaController = require('./controllers/tarefaController.js')
+// Cross origin recurse sharing - compartilhamento recursivo de origem cruzada "Diferentes" 
+// Usado para aceitar requisições vindas de origens diferentes, ou seja, qualquer outro http .... diferente do nosso.
+app.use(cors())
 
+// importando os controllers
+const usuarioController = require('./controllers/usuario-controller.js')
+const tarefaController = require('./controllers/tarefa-controller.js')
+
+// Chama as rotas
 usuarioController.rotas(app)
 tarefaController.rotas(app)
 
+// Exporta o app para ser usado em outro módulo, por exemplo: no server.js
 module.exports = app
